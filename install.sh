10df4c0b0f572cf67afa613b4f40b58ffbf1e63a
Patrick Ecker <ryyppy@users.noreply.github.com>
2026-06-11 11:03:24 +0200

    Add lazygit dotfile config

--- install.sh
+++ install.sh
@@ -13,3 +13,18 @@ cd ~
 [[ ! -s ~/.config ]] && mkdir ~/.config
 [[ ! -s ~/.config/nvim ]] && ln -s $DIR/nvim ~/.config/nvim
 [[ ! -s ~/.config/starship.toml ]] && ln -s $DIR/starship.toml ~/.config/starship.toml
+
+if [[ "$(uname)" == "Darwin" ]]; then
+  LAZYGIT_CONFIG_DIR="$HOME/Library/Application Support/lazygit"
+else
+  LAZYGIT_CONFIG_DIR="$HOME/.config/lazygit"
+fi
+
+mkdir -p "$LAZYGIT_CONFIG_DIR"
+LAZYGIT_CONFIG="$LAZYGIT_CONFIG_DIR/config.yml"
+if [[ ! -e "$LAZYGIT_CONFIG" ]]; then
+  ln -s "$DIR/lazygit/config.yml" "$LAZYGIT_CONFIG"
+elif [[ -f "$LAZYGIT_CONFIG" && ! -s "$LAZYGIT_CONFIG" && ! -L "$LAZYGIT_CONFIG" ]]; then
+  rm "$LAZYGIT_CONFIG"
+  ln -s "$DIR/lazygit/config.yml" "$LAZYGIT_CONFIG"
+fi
